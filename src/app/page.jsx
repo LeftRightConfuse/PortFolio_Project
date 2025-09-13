6b53aa9b6629c798c85dfcc2349ebcc72f0a6ead
lamzacknon <66200059@kmitl.ac.th>
2025-09-13 17:29:17 +0700

    about more

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,22 +10,22 @@ export default function Home() {
         {/* ฝั่งซ้าย (ข้อความ) */}
         <div className="flex-1 flex flex-col justify-center">
           <div className="relative flex items-center">
-            <p className="text-white font-mono text-5xl font-bold relative z-10 pl-20">Hello</p>
-            <div className="absolute left-57 top-8 w-2 h-2 bg-blue-500 rounded-full z-0"></div>
+            <p className="text-white font-mono text-6xl font-bold relative z-10 pl-20">Hello</p>
+            <div className="absolute left-66 top-10 w-2.5 h-2.5 bg-blue-500 rounded-full z-0"></div>
           </div>
 
           <div className="flex items-center mt-10">
             <p className="text-blue-600 text-4xl font-bold mr-5">______</p>
-            <p className="text-white font-mono text-4xl font-bold">
+            <p className="text-white font-mono text-5xl font-bold">
               I'm Chisanupong
             </p>
           </div>
 
-          <p className="text-white font-mono text-5xl font-bold mt-10 pl-20">
+          <p className="text-white font-mono text-6xl font-bold mt-10 pl-20">
             Fullstack Developer
           </p>  
 
-          <p className="text-white font-mono text-3xl mt-10">
+          <p className="text-white font-mono text-4xl mt-10">
             Welcome to my portfolio website!
           </p>
         </div>
@@ -35,7 +35,7 @@ export default function Home() {
           <img
             src="./profile.JPG"
             alt="Profile Picture"
-            className="w-[60%] max-w-md rounded-full object-cover shadow-lg"/>
+            className="w-[60%] rounded-full object-cover shadow-lg"/>
         </div>
       </div>
       <div className="text-2xl font-Poppins flex items-center justify-center gap-15 h-20 bg-gray-900">
@@ -64,7 +64,27 @@ export default function Home() {
           SQL
         </span>
       </div>
-
+      <div className="flex h-screen w-full bg-gradient-to-r from-gray-600 from-gray-70 from-gray-900 py-10 px-10">
+        <div className="flex1 flex m-auto text-white">
+          <div className="block w-full">
+            <div className='flex items-center'>
+              <img
+                src="./webdeslogo.png"
+                alt="Website Design Picture"
+                className="w-[8%] rounded-full object-cover shadow-lg"/>
+              <p className='text-2xl font-mono font-bold ml-10'>Website Design</p>
+            </div>
+            <div className='flex mt-20 items-center'>
+              <img
+                src="./webdevlogo.jpg"
+                alt="Website Design Picture"
+                className="w-[8%] rounded-full object-cover shadow-lg"/>
+              <p className='text-2xl font-mono font-bold ml-10'>Website Development</p>
+            </div>
+          </div>
+        </div>
+          
+      </div>
     </div>
   );
 }
