5d01f84719c6a47bbdffdc152624b42a2c4048a7
lamzacknon <66200059@kmitl.ac.th>
2025-09-16 16:59:51 +0700

    page project +about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,13 @@
-import Nav from '../app/components/Nav';
+import Nav from './components/Nav';
+import Link from "next/link";
+import { FiExternalLink } from "react-icons/fi";
 
 export default function Home() {
   return (
     <div className='content'>
       <Nav />
-      {/* container หลัก ใช้ flex */}
-      <div className="flex h-screen w-full bg-gradient-to-r from-gray-600 from-gray-70 from-gray-900 py-10 px-10">
-        
-        {/* ฝั่งซ้าย (ข้อความ) */}
+      <div className="flex h-screen w-full bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800 py-10 px-10">
+
         <div className="flex-1 flex flex-col justify-center">
           <div className="relative flex items-center">
             <p className="text-white font-mono text-6xl font-bold relative z-10 pl-20">Hello</p>
@@ -15,7 +15,7 @@ export default function Home() {
           </div>
 
           <div className="flex items-center mt-10">
-            <p className="text-blue-600 text-4xl font-bold mr-5">______</p>
+            <p className="text-blue-500 text-4xl font-bold mr-5">______</p>
             <p className="text-white font-mono text-5xl font-bold">
               I'm Chisanupong
             </p>
@@ -30,7 +30,6 @@ export default function Home() {
           </p>
         </div>
 
-        {/* ฝั่งขวา (รูป) */}
         <div className="flex-1 flex justify-center items-center">
           <img
             src="./profile.JPG"
@@ -39,6 +38,12 @@ export default function Home() {
         </div>
       </div>
       <div className="text-2xl font-Poppins flex items-center justify-center gap-15 h-20 bg-gray-900">
+        <div className="w-full h-px max-w-6xl mx-auto py-1"
+              style={{
+                backgroundImage:
+                    "linear-gradient(90deg, rgba(149, 131, 198, 0) 1.46%, rgba(149, 131, 198, 0.6) 40.83%, rgba(149, 131, 198, 0.3) 65.57%, rgba(149, 131, 198, 0) 107.92%)",
+                }}
+              ></div>
         <span className="px-4 py-2 rounded-4xl bg-white/10 backdrop-blur-md border border-white/20 text-white shadow-lg">
           HTMLS
         </span>
@@ -63,28 +68,125 @@ export default function Home() {
         <span className="px-4 py-2 rounded-4xl bg-white/10 backdrop-blur-md border border-white/20 text-white shadow-lg">
           SQL
         </span>
+        <div className="w-full h-px max-w-6xl mx-auto py-1"
+              style={{
+                backgroundImage:
+                    "linear-gradient(90deg, rgba(149, 131, 198, 0) 1.46%, rgba(149, 131, 198, 0.6) 40.83%, rgba(149, 131, 198, 0.3) 65.57%, rgba(149, 131, 198, 0) 107.92%)",
+                }}
+              ></div>
       </div>
-      <div className="flex h-screen w-full bg-gradient-to-r from-gray-600 from-gray-70 from-gray-900 py-10 px-10">
-        <div className="flex1 flex m-auto text-white">
-          <div className="block w-full">
-            <div className='flex items-center'>
+      <div className="flex h-screen w-full bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800 py-10 px-10">
+        <div className="flex m-auto text-white w-full">
+          <div className="basis-[40%]">
+            <div className="ml-30 flex items-center">
               <img
-                src="./webdeslogo.png"
+                src="./webdeslogo.jpg"
                 alt="Website Design Picture"
-                className="w-[8%] rounded-full object-cover shadow-lg"/>
-              <p className='text-2xl font-mono font-bold ml-10'>Website Design</p>
+                className="w-[15%] rounded-full object-cover shadow-lg"
+              />
+              <p className="text-2xl font-mono font-bold ml-10">Website Design</p>
             </div>
-            <div className='flex mt-20 items-center'>
+            <div className="ml-30 flex mt-20 items-center">
               <img
                 src="./webdevlogo.jpg"
-                alt="Website Design Picture"
-                className="w-[8%] rounded-full object-cover shadow-lg"/>
-              <p className='text-2xl font-mono font-bold ml-10'>Website Development</p>
+                alt="Website Development Picture"
+                className="w-[15%] rounded-full object-cover shadow-lg"
+              />
+              <p className="text-2xl font-mono font-bold ml-10">Website Development</p>
             </div>
           </div>
+          <div className="basis-[60%] pl-10">
+            <p className="text-white font-mono font-bold text-5xl">About me</p>
+            <p className="mt-10 text-gray-300 font-mono text-xl">
+              I started my journey as a developer by practicing HTML and CSS, which sparked 
+            </p>
+            <p className="mt-3 text-gray-300 font-mono text-xl">
+              my passion for web development. As I explored further, I experimented with
+            </p>
+            <p className="mt-3 text-gray-300 font-mono text-xl">
+              projects and gradually advanced my skills. Eventually, I learned and applied
+            </p>
+            <p className="mt-3 text-gray-300 font-mono text-xl">  
+              frameworks such as React and Next.js, which enabled me to develop this website.
+            </p>
+          </div>
         </div>
-          
+      </div>
+        <div className="flex flex-col items-center justify-center h-screen w-full bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800">
+          <h2 className="text-5xl font-mono font-bold text-white">Projects</h2>
+          <div className="mt-7 flex flex-col items-center">
+            <div className="w-[4px] h-15 bg-gradient-to-b from-blue-500 to-blue-300 relative">
+              <div className="absolute -bottom-3 left-1/2 -translate-x-1/2 w-2 h-2 bg-blue-300 rounded-full mt-10"></div>
+            </div>
+          </div>
+          <div className="mt-30 text-white flex m-auto">
+            <div className=" text-grey-200 basis-[60%] ml-40">
+            <p className='text-3xl font-mono font-bold'>Booking</p>
+              <div className="text-xl flex items-center gap-10 h-20">
+                <span className="px-4 py-2 rounded-4xl bg-white/10 backdrop-blur-md border border-white/20 text-white shadow-lg">
+                  MongoDB
+                </span>
+                <span className="px-4 py-2 rounded-4xl bg-white/10 backdrop-blur-md border border-white/20 text-white shadow-lg">
+                  Express
+                </span>
+                <span className="px-4 py-2 rounded-4xl bg-white/10 backdrop-blur-md border border-white/20 text-white shadow-lg">
+                  Node.js
+                </span>
+                <span className="px-4 py-2 rounded-4xl bg-white/10 backdrop-blur-md border border-white/20 text-white shadow-lg">
+                  React
+                </span>
+              </div>
+              <p className="mt-5 text-gray-300 font-mono text-xl">
+                A web application that allows users to book classrooms, meeting rooms
+              </p>
+              <p className="mt-3 text-gray-300 font-mono text-xl">
+                and laboratories. It features a user-friendly interface that simplifies
+              </p>
+              <p className="mt-3 text-gray-300 font-mono text-xl">
+                room selection and booking management.
+              </p>
+              <div className='mt-5 flex gap-5'>
+                <Link
+                  href="https://github.com/nnimkanSong/Project_Webapp.git"
+                  target="_blank"
+                  className="font-mono inline-flex items-center gap-2 px-5 py-2 rounded-full 
+                  border border-white text-white bg-transparent
+                  hover:bg-blue-500/10 active:bg-blue-500/20
+                  transition-colors duration-200">
+                  GitHub
+                </Link>
+                <Link
+                  href="https://youtu.be/dQw4w9WgXcQ?si=3x2QGMfIiYHhuqbx"
+                  target="_blank"
+                  className="font-mono ml-5 group inline-flex items-center gap-2 text-white font-medium relative"
+                >
+                  View project
+                  <FiExternalLink className="w-4 h-4" />
+                  <span className="absolute left-0 -bottom-1 w-0 h-[1px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
+                </Link>
+                <div className="ml-40 w-96 gap-5 px-5 py-2 rounded-full flex border border-white">
+                  <p className='font-mono'>Progress</p>
+                  <div className="w-full bg-gray-700 rounded-full h-6 items-center">
+                    <div
+                      className="bg-green-500 h-6 rounded-full text-center font-mono"
+                      style={{ width: "70%" }}
+                    >70%
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            <div className=" text-white basis-[40%]">
+                <img
+                src="./bookinglogin.png"
+                alt="bookinglogin Picture"
+                className="w-[70%] object-cover px-6 py-6 rounded-4xl bg-white/10 backdrop-blur-md border border-white/20 shadow-lg"
+              />
+            </div>
+        </div>                
       </div>
     </div>
+      
   );
 }
