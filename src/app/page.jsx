37a5623cec2ffb81ad14e41bcee72bd7d32338a0
lamzacknon <lamzacknon1@gmail.com>
2025-09-23 01:23:52 +0700

    test responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,27 +15,27 @@ export default function Home() {
   return (
     <main className="content">
       <Nav />
-      <section id="home" className="flex h-screen w-full bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800 py-10 px-10">
-        <div className="flex-1 flex flex-col justify-center">
-          <div className="relative flex items-center">
-            <p className="text-white font-mono text-6xl font-bold relative z-10 pl-20">
+      <section id="home" className="flex flex-col xl:flex-row h-screen w-full bg-gradient-to-r from-gray-950 via-gray-900 to-gray-800 py-10 px-10">
+        <div className="flex-1 flex flex-col justify-center item-center">
+          <div className="relative flex items-center">       
+            <p className="text-white mt-20 font-mono text-4xl xl:text-6xl font-bold relative z-10 pl-10 xl:pl-20">
               Hello
             </p>
-            <div className="absolute left-66 top-10 w-2.5 h-2.5 bg-blue-500 rounded-full z-0"></div>
+            <div className="absolute left-37 top-27 xl:left-66 xl:top-30 w-1.5 h-1.5 xl:w-2.5 xl:h-2.5 bg-blue-500 rounded-full z-0"></div>
           </div>
 
           <div className="flex items-center mt-10">
-            <p className="text-blue-500 text-4xl font-bold mr-5">______</p>
-            <p className="text-white font-mono text-5xl font-bold">
+            <p className="text-blue-500 text-2xl xl:text-4xl font-bold mr-5">______</p>
+            <p className="text-white font-mono text-3xl xl:text-5xl font-bold">
               I'm Chisanupong
             </p>
           </div>
 
-          <p className="text-white font-mono text-6xl font-bold mt-10 pl-20">
+          <p className="text-white font-mono text-4xl xl:text-6xl font-bold mt-10 pl-20">
             Fullstack Developer
           </p>
 
-          <p className="text-white font-mono text-4xl mt-10">
+          <p className="text-white font-mono text-2xl xl:text-4xl mt-10">
             Welcome to my portfolio website!
           </p>
         </div>
